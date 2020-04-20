436546aba0285539488f26bfaed7ee83554d69a5
enoshima7 <duanjipeng0328@gmail.com>
2020-04-20 16:52:46 +0800

    finish

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ interface ItemData {
   name: string;
   price: number;
 }
-type OnCheckedChange<T> = (item: T, checked: boolean) => any;
+
 type CheckedMap = {
   [id: number]: boolean;
 };
@@ -15,6 +15,7 @@ export const App = () => {
   const cartData = UseRequest(url);
   // 商品勾选
   const [checkedMap, setCheckedMap] = useState<CheckedMap>({});
+  type OnCheckedChange<T> = (item: T, checked: boolean) => any;
   const onCheckedChange: OnCheckedChange<ItemData> = (cartItem, checked) => {
     const { id } = cartItem;
     const newCheckedMap = Object.assign({}, checkedMap, {
@@ -38,22 +39,50 @@ export const App = () => {
     return cartItems.reduce((sum, cur) => sum + cur.price, 0);
   };
   let a = filterChecked();
-  console.log(a);
   const total1 = a.reduce((sum, item) => {
-    console.log(item);
     if (item) {
       return sum + item.price;
     } else {
       return 0;
     }
   }, 0);
+
+  const checkedAll =
+    cartData.length !== 0 && filterChecked().length === cartData.length;
+  type OnCheckedAllChange = (status: boolean) => any;
+  const onCheckedAllChange: OnCheckedAllChange = (status) => {
+    // 构造新的勾选map
+    let newCheckedMap: CheckedMap = {};
+    // 全选
+    if (status) {
+      cartData.forEach((cartItem) => {
+        newCheckedMap[cartItem.id] = true;
+      });
+    }
+    // 取消全选的话 直接把map赋值为空对象
+    setCheckedMap(newCheckedMap);
+  };
   return (
     <div>
       {cartData.map((v) => {
-        return <Item key={v.id} item={v} onCheckedChange={onCheckedChange} />;
+        const checked = checkedMap[v.id] || false;
+        return (
+          <Item
+            key={v.id}
+            item={v}
+            checked={checked}
+            onCheckedChange={onCheckedChange}
+          />
+        );
       })}
       <div className='item'>
-        <input type='checkbox' />
+        <input
+          type='checkbox'
+          checked={checkedAll}
+          onChange={(e) => {
+            onCheckedAllChange(e.target.checked);
+          }}
+        />
         <span>全选</span>
         <span>总价{total1}</span>
       </div>
--- src/Item.tsx
+++ src/Item.tsx
@@ -8,15 +8,17 @@ interface ItemData {
 type OnCheckedChange<T> = (item: T, checked: boolean) => any;
 interface Props {
   item: ItemData;
+  checked: boolean;
   onCheckedChange: OnCheckedChange<ItemData>;
 }
 
-export const Item: React.FC<Props> = ({ item, onCheckedChange }) => {
+export const Item: React.FC<Props> = ({ item, onCheckedChange, checked }) => {
   const { name, price, id } = item;
   return (
     <div className='item'>
       <input
         type='checkbox'
+        checked={checked}
         name={id + ''}
         onChange={(e) => {
           onCheckedChange(item, e.target.checked);
